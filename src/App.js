ff1d2eee284755a191c365ca013746493e4ca48c
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-12 20:14:55 +0530

    json-server setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => { 
+    fetchData()
+  }, [])
+  const fetchData = () => { 
+    axios.get("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
